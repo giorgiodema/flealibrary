ba853c564ac45e33993b8a54448582cb1f4fca35
giorgiodema <giorgiodema@gmail.com>
2018-05-31 15:49:39 +0200

    img_resize

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531110619) do
+ActiveRecord::Schema.define(version: 7) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
