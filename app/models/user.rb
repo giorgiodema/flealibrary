a76704b8fe978a09ec6452a69831dcc49d40125c
iaiamomo <flavia.monti95@gmail.com>
2018-06-02 15:23:39 +0200

    correzioni

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         #:confirmable
+         :rememberable, :trackable, :validatable
+         #:confirmable, :recoverable
 
   #validazione di attributi
   validates :name, presence: true
