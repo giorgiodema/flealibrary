40b0683fe3246a4e796d4816454a614f0b3a0780
iaiamomo <flavia.monti95@gmail.com>
2018-06-04 13:10:59 +0200

    login feature

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -1,8 +1,49 @@
-
-Given "I am a booklover" do
-    @booklover = User.create(:name=>'name_test', :surname=>'surname_test', :username=>'username_test', :email=>'email@test.it', :password=>'password', :cap=>'00000', :radius=>'0')
+def fill_element(name, value)
+    element = page.find(name)
+    element.set(value)
 end
 
-When "I login" do |:email|
-    @user = User.find_by()
+def login
+    visit '/users/sign_in'
+    fill_in "Email", :with => @user.email
+    fill_in "Password", :with => @user.password
+    click_button('Log in session')
+end
+
+def create_user
+    @user = User.create(:name=>'name_test', :surname=>'surname_test', :username=>'username_test', :email=>'email@test.it', :password=>'password', :cap=>'00000', :radius=>'0')
+end
+
+def find_user
+    @user = User.where(username: 'username_test').first
+end
+
+#GIVEN
+Given /^I exist as a user$/ do
+    create_user
+end
+
+Given /^I am not logged in$/ do
+    page.has_content?('Log in')
+end
+
+#WHEN
+When /^I login with valid credentials/ do
+    find_user
+    login
+end
+
+#THEN
+Then /^I see a successful sign in message/ do
+    page.has_content?("Signed in successfully.")
+end
+
+Then /^I should be signed in$/ do
+    page.has_content? "Log out"
+    page.has_no_content? "Sign up"
+    page.has_no_content? "Log in"
+end
+
+Then /^I should be in profile/ do
+    page.has_content? "Profile"
 end
